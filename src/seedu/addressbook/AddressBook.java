175fe3b2df9266dfd05493cc0cda1be967abef2c
CHQ\Ching <hqching@gmail.com>
2017-01-27 10:48:24 +0800

    update README and javadocs

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -108,7 +108,7 @@ public class AddressBook {
 
     private static final String COMMAND_FIND_WORD = "find";
     private static final String COMMAND_FIND_DESC = "Finds all persons whose names contain any of the specified "
-            + "keywords (case-sensitive) and displays them as a list with index numbers.";
+            + "keywords (NOT case-sensitive) and displays them as a list with index numbers.";
     private static final String COMMAND_FIND_PARAMETERS = "KEYWORD [MORE_KEYWORDS]";
     private static final String COMMAND_FIND_EXAMPLE = COMMAND_FIND_WORD + " alice bob charlie";
 
@@ -499,11 +499,11 @@ public class AddressBook {
 
     /**
      * Retrieves all persons in the full model whose names contain some of the
-     * specified keywords.
+     * specified keywords. Case insensitive.
      *
      * @param keywords for searching
      * @return list of persons in full model with name containing some of the
-     *         keywords
+     *         keywords 
      */
     private static ArrayList<String[]> getPersonsWithNameContainingAnyKeyword(Collection<String> keywords) {
         final ArrayList<String[]> matchedPersons = new ArrayList<>();
@@ -634,8 +634,8 @@ public class AddressBook {
     private static String executeSortAllPersonsInAddressBook() {
         ArrayList<String[]> toBeDisplayed = getAllPersonsInAddressBook();
         class StringArrayComparator implements Comparator<String[]> {
-            public int compare(String[] string1, String[] string2) {
-                return string1[0].toLowerCase().compareTo(string2[0].toLowerCase());
+            public int compare(String[] currString, String[] otherString) {
+                return currString[0].toLowerCase().compareTo(otherString[0].toLowerCase());
             }
         }
         StringArrayComparator nameComp = new StringArrayComparator();
